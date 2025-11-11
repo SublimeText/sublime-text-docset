fc51fd925e1573f4ac5afd9539249622ccb6b3f8
Michael Lyons <github@michael.lyo.nz>
2025-11-11 15:55:29 -0500

    Test half the Sublime Text pages. Fix some.

--- test/test_docset.py
+++ test/test_docset.py
@@ -57,6 +57,7 @@ class DocsetTestCaseBase(ABC, unittest.TestCase):
         self, path: str,
         contains_strict: list[tuple[str,str]],
         contains_lenient: list[tuple[str,str]] | None = None,
+        not_contains: list[tuple[str,str]] | None = None,
     ):
         sql = f'''
             SELECT  type, name
@@ -79,6 +80,9 @@ class DocsetTestCaseBase(ABC, unittest.TestCase):
             ds_type, ds_term = pair
             lookup_result = lookup[ds_term][:]
             self.assertFalse(lookup_result.remove(ds_type))
+
+        for pair in not_contains or []:
+            self.assertNotIn(pair, items)
 
 
 class SublimeMergeDocsetTestCase(DocsetTestCaseBase):
@@ -146,6 +150,16 @@ class SublimeMergeDocsetTestCase(DocsetTestCaseBase):
 class SublimeTextDocsetTestCase(DocsetTestCaseBase):
     NAME = 'sublime-text.docset'
 
+    def test_home(self):
+        contains = [
+            ('Guide', 'Documentation'),
+            ('Section', 'Usage'),
+            ('Section', 'Customization'),
+            ('Section', 'Miscellaneous'),
+            ('Section', 'Package Development'),
+        ]
+        self._test_a_doc_page_index('docs/index.html', contains)
+
     def test_git_integration(self):
         contains = [
             ('Guide', 'Git Integration'),
@@ -153,8 +167,119 @@ class SublimeTextDocsetTestCase(DocsetTestCaseBase):
             ('Section', 'Diff Markers'),
             ('Command', 'Open Git Repository…'),
             ('Command', 'Sublime Merge: Folder History'),
+            ('Setting', 'show_git_status'),
         ]
         self._test_a_doc_page_index('docs/git_integration.html', contains)
+
+    def test_incremental_diff(self):
+        contains = [
+            ('Guide', 'Incremental Diff'),
+            ('Section', 'Diff Markers'),
+            ('Tag', 'diff.inserted'),
+            ('Setting', 'mini_diff'),
+        ]
+        self._test_a_doc_page_index('docs/incremental_diff.html', contains)
+
+    def test_indexing(self):
+        contains = [
+            ('Guide', 'Indexing'),
+            ('Section', 'Status'),
+            ('Setting', 'index_exclude_patterns'),
+        ]
+        self._test_a_doc_page_index('docs/indexing.html', contains)
+
+    def test_completions(self):
+        contains = [
+            ('Guide', 'Completions'),
+            ('Section', 'Completion Metadata'),
+            ('Setting', 'auto_complete_delay'),
+            ('Field', 'tabTrigger'),
+            ('Value', 'Navigation'),
+            ('Value', '"navigation"'),
+            ('Variable', '$TM_FILEPATH'),
+        ]
+        self._test_a_doc_page_index('docs/completions.html', contains)
+
+    def test_distraction_free_mode(self):
+        contains = [
+            ('Guide', 'Distraction Free Mode'),
+            ('Section', 'Customization'),
+            ('Setting', '"gutter"'),
+            ('File', 'Packages/User/Distraction Free.sublime-settings'),
+        ]
+        self._test_a_doc_page_index('docs/distraction_free.html', contains)
+
+    def test_projects(self):
+        contains = [
+            ('Guide', 'Projects'),
+            ('Section', 'Project Format'),
+            ('Setting', 'follow_symlinks'),
+        ]
+        self._test_a_doc_page_index('docs/projects.html', contains)
+
+    def test_settings(self):
+        contains = [
+            ('Guide', 'Settings'),
+            ('Section', 'Categories'),
+            ('File', 'Packages/Default/Preferences.sublime-settings'),
+        ]
+        self._test_a_doc_page_index('docs/settings.html', contains)
+
+    def test_key_bindings(self):
+        contains = [
+            ('Guide', 'Key Bindings'),
+            ('Section', 'User Bindings'),
+            ('Filter', '"has_prev_field"'),
+            ('Setting', '"args" Key'),
+        ]
+        self._test_a_doc_page_index('docs/key_bindings.html', contains)
+
+    def test_indentation_settings(self):
+        contains = [
+            ('Guide', 'Indentation Settings'),
+            ('Section', 'Converting Between Tabs and Spaces'),
+            ('Setting', 'indent_to_bracket'),
+            ('Setting', 'use_tab_stops'),
+        ]
+        self._test_a_doc_page_index('docs/indentation.html', contains)
+
+    def test_spell_checking(self):
+        contains = [
+            ('Guide', 'Spell Checking'),
+            ('Section', 'Dictionaries'),
+            ('Setting', 'spell_check'),
+            ('Command', 'next_misspelling'),
+        ]
+        not_contains = [
+            ('Command', 'word'),
+        ]
+        self._test_a_doc_page_index('docs/spell_checking.html', contains,
+                                    not_contains=not_contains)
+
+    def test_build_systems(self):
+        contains = [
+            ('Guide', 'Build Systems'),
+            ('Section', 'Custom Options'),
+            ('Option', 'quiet'),
+            ('Variable', '$project_path'),
+        ]
+        self._test_a_doc_page_index('docs/build_systems.html', contains)
+
+    def test_packages(self):
+        contains = [
+            ('Guide', 'Packages'),
+            ('Section', 'Locations'),
+            ('File', '<data_path>/Installed Packages/'),
+        ]
+        self._test_a_doc_page_index('docs/packages.html', contains)
+
+    def test_file_patterns(self):
+        contains = [
+            ('Guide', 'File Patterns'),
+            ('Section', 'Uses'),
+            ('Setting', '"binary_file_patterns"'),
+        ]
+        self._test_a_doc_page_index('docs/file_patterns.html', contains)
 
     def test_api_reference(self):
         contains = [
